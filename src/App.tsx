05c638e5c2fccf476621f5ff0d9df80d1f91dc77
Nofrialdi <aldi.fasive@gmail.com>
2023-06-08 11:27:32 +0700

    bug liked

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { ButtonJoin } from "./components/Button/ButtonJoin";
-import { ListGallery } from "./components/Contents/ListGallery";
+import { useState } from "react";
+import { Galleries } from "./data/DataGalleries";
+import { GalleryCard } from "./components/Contents/GalleryCard";
 
 function App() {
+  const [galleries, setGalleries] = useState(Galleries);
+
+  function LikeGallery(id: number, isLiked: boolean) {
+    const newGalery = galleries.map((galleries) => {
+      if (galleries.id === id) {
+        galleries.isLiked = isLiked;
+      }
+      return galleries;
+    });
+    setGalleries(newGalery);
+  }
   return (
     <div className="flex flex-col items-center justify-center">
       <Header />
-      <ListGallery />
+      <div className="mt-4 max-w-[900px] flex flex-wrap flex-row justify-center items-center gap-4">
+        {galleries.map((gallery) => (
+          <GalleryCard id={gallery.id} isLiked={gallery.isLiked} name={gallery.name} imageUrl={gallery.imageUrl} LikeGallery={LikeGallery} />
+        ))}
+      </div>
       <ButtonJoin />
     </div>
   );
